28751c3a62aa72ba0b9cc968ea988a2b5be0a28b
YUSHINSHUB <kus9087@naver.com>
2023-04-12 14:34:54 +0900

    Q10093 NUMBER (IMPLEMENTATION, BRONZE 2) ADDED

--- Implementation/Q10093_Number_B2.java
+++ Implementation/Q10093_Number_B2.java
@@ -1,10 +1,8 @@
-package aaNotepad;
+package Implementation;
 
 import java.io.*;
-import java.util.*;
-import java.text.*;
 
-public class notepad {
+public class Q10093_Number_B2 {
 
 	public static void main(String[] args) throws IOException {
 
