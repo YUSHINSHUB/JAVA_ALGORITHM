97eedbf5d6644141ebbf21d18577c69afac837d7
Yusin <kus9087@naver.com>
2023-04-26 17:00:29 +0900

    Q10812 Rearrange Basket (IMPLEMENTATION, BRONZE 2) ADDED

--- Implementation/Q10812_Rearrange_Basket_B2.java
+++ Implementation/Q10812_Rearrange_Basket_B2.java
@@ -1,10 +1,8 @@
-package aaNotepad;
+package Implementation;
 
 import java.io.*;
-import java.util.*;
-import java.text.*;
 
-public class notepad {
+public class Q10812_Rearrange_Basket_B2 {
 
 	public static void main(String[] args) throws IOException {
 
@@ -14,36 +12,37 @@ public class notepad {
 		String inp[] = br.readLine().split(" ");
 		int n = Integer.parseInt(inp[0]);
 		int m = Integer.parseInt(inp[1]);
-		
-		int bas[] = new int[n+1];
-		
-		for( int i = 1 ; i <= n ; i++ ) {
+
+		int bas[] = new int[n + 1];
+
+		for (int i = 1; i <= n; i++) {
 			bas[i] = i;
 		}
-		
-		for( int l = 0 ; l < m ; l++ ) {
+
+		for (int l = 0; l < m; l++) {
 			inp = br.readLine().split(" ");
 			int i = Integer.parseInt(inp[0]);
 			int j = Integer.parseInt(inp[1]);
 			int k = Integer.parseInt(inp[2]);
-			
-			int temp[] = new int[n+1];
-			for (int o = 1 ; o <= n ; o++ ) temp[o] = bas[o];
+
+			int temp[] = new int[n + 1];
+			for (int o = 1; o <= n; o++)
+				temp[o] = bas[o];
 			int idx = i;
-			
-			for (int o = k ; o <= j ; o++ ) {
+
+			for (int o = k; o <= j; o++) {
 				bas[idx] = temp[o];
 				idx++;
 			}
-			
-			for( int o = i ; o < k ; o++ ) {
+
+			for (int o = i; o < k; o++) {
 				bas[idx] = temp[o];
 				idx++;
 			}
-			
+
 		}
-		
-		for (int i = 1 ; i <= n ; i++ ) {
+
+		for (int i = 1; i <= n; i++) {
 			bw.write(bas[i] + " ");
 		}
 
