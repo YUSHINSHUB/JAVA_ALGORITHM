21c8fce32f72131163b7f8bf754b1b3795846003
Yusin <kus9087@naver.com>
2023-05-01 13:15:14 +0900

    Q1251 WORD SEGMENTATION (STRING, SILVER 5) ADDED

---
+++ String/Q1251_Word_Segmentation_S5.java
@@ -0,0 +1,34 @@
+package String;
+
+import java.io.*;
+import java.util.*;
+
+public class Q1251_Word_Segmentation_S5 {
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		String s = br.readLine();
+		ArrayList<String> list = new ArrayList<String>();
+
+		for (int i = 0; i < s.length() - 2; i++) {
+			for (int j = i + 1; j < s.length() - 1; j++) {
+				StringBuilder a = new StringBuilder(s.substring(0, i + 1)).reverse();
+				StringBuilder b = new StringBuilder(s.substring(i + 1, j + 1)).reverse();
+				StringBuilder c = new StringBuilder(s.substring(j + 1, s.length())).reverse();
+				a.append(b);
+				a.append(c);
+				list.add(a.toString());
+			}
+		}
+
+		Collections.sort(list);
+
+		bw.write(list.get(0));
+
+		bw.flush();
+		bw.close();
+	}
+}
--- aaNotepad/notepad.java
+++ aaNotepad/notepad.java
@@ -11,64 +11,23 @@ public class notepad {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-		String pass = "";
-		String vowel = "aeiou";
-
-		while (true) {
-
-			pass = br.readLine();
-			if (pass.equals("end"))
-				break;
-
-			boolean b1 = false, b2 = true, b3 = true;
-
-			if (pass.length() == 1) {
-				if (vowel.contains(pass.charAt(0) + ""))
-					b1 = true;
-			} else if (pass.length() == 2) {
-				if (vowel.contains(pass.charAt(0) + "") || vowel.contains(pass.charAt(1) + ""))
-					b1 = true;
-				if (pass.charAt(0) == pass.charAt(1) && pass.charAt(0) != 'e' && pass.charAt(0) != 'o') {
-					b3 = false;
-				}
-			} else {
-				if (vowel.contains(pass.charAt(0) + "") || vowel.contains(pass.charAt(1) + ""))
-					b1 = true;
-				if (pass.charAt(0) == pass.charAt(1) && pass.charAt(0) != 'e' && pass.charAt(0) != 'o') {
-					b3 = false;
-				}
-
-				if (b3) {
-					for (int i = 2; i < pass.length(); i++) {
-
-						if (vowel.contains(pass.charAt(i) + ""))
-							b1 = true;
-
-						if (vowel.contains(pass.charAt(i) + "") && vowel.contains(pass.charAt(i - 1) + "")
-								&& vowel.contains(pass.charAt(i - 2) + "")) {
-							b2 = false;
-							break;
-						} else if (!vowel.contains(pass.charAt(i) + "") && !vowel.contains(pass.charAt(i - 1) + "")
-								&& !vowel.contains(pass.charAt(i - 2) + "")) {
-							b2 = false;
-							break;
-						}
-
-						if (pass.charAt(i) == pass.charAt(i - 1) && pass.charAt(i) != 'e' && pass.charAt(i) != 'o') {
-							b3 = false;
-							break;
-						}
-
-					}
-				}
+		String s = br.readLine();
+		ArrayList<String> list = new ArrayList<String>();
+		
+		for( int i = 0 ; i < s.length()-2 ; i++ ) {
+			for( int j = i+1 ; j < s.length()-1 ; j++ ) {
+					StringBuilder a = new StringBuilder(s.substring(0, i+1)).reverse();
+					StringBuilder b = new StringBuilder(s.substring(i+1, j+1)).reverse();
+					StringBuilder c = new StringBuilder(s.substring(j+1, s.length())).reverse();
+					a.append(b);
+					a.append(c);
+					list.add(a.toString());
 			}
-
-			if (b1 && b2 && b3)
-				bw.write("<" + pass + "> is acceptable.\n");
-			else
-				bw.write("<" + pass + "> is not acceptable.\n");
-
 		}
+		
+		Collections.sort(list);
+		
+		bw.write(list.get(0));
 
 		bw.flush();
 		bw.close();
