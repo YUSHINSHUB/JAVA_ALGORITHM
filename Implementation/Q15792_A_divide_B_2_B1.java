547910934eeb6c8a8ffee57df37995434411d592
Yusin <kus9087@naver.com>
2023-05-20 12:24:50 +0900

    Q15792 A/B - 2 (IMPLEMENTATION, BRONZE 1) ADDED

--- Implementation/Q15792_A_divide_B_2_B1.java
+++ Implementation/Q15792_A_divide_B_2_B1.java
@@ -1,10 +1,10 @@
-package aaNotepad;
+package Implementation;
 
 import java.io.*;
 import java.math.*;
 import java.util.*;
 
-public class notepad {
+public class Q15792_A_divide_B_2_B1 {
 
 	public static void main(String[] args) throws IOException {
 
@@ -13,10 +13,11 @@ public class notepad {
 
 		String inp[] = br.readLine().split(" ");
 
-		BigDecimal bd1 = new BigDecimal(inp[0] + ".0");
-		BigDecimal bd2 = new BigDecimal(inp[1] + ".0");
+		BigDecimal bd1 = new BigDecimal(inp[0]);
+		BigDecimal bd2 = new BigDecimal(inp[1]);
 
 		bd1 = bd1.divide(bd2, 1002, BigDecimal.ROUND_DOWN);
+		// BigDecimal a.divide(BigDecimal b, n번째 자리까지 표시, ROUND DOWN (버림 // 필요에 따라 올림, 반올림 등의 기능도 있음) )
 		bw.write(bd1.toString());
 
 		bw.flush();
