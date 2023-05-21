cb5f6c6cbc3fa26076cf5e71e83223d6cba054d4
Yusin <kus9087@naver.com>
2023-05-21 12:31:38 +0900

    Q2033 ROUNDING (MATHEMATICS, BRONZE 1) ADDED

--- Mathematics/Q2033_Rounding_B1.java
+++ Mathematics/Q2033_Rounding_B1.java
@@ -1,10 +1,8 @@
-package aaNotepad;
+package Mathematics;
 
 import java.io.*;
-import java.math.*;
-import java.util.*;
 
-public class notepad {
+public class Q2033_Rounding_B1 {
 
 	public static void main(String[] args) throws IOException {
 
@@ -13,12 +11,12 @@ public class notepad {
 
 		int n = Integer.parseInt(br.readLine());
 		int idx = 10;
-		
-		while( n > idx ) {
-			n = (n + (idx/10*5) ) / idx * idx;
+
+		while (n > idx) {
+			n = (n + (idx / 10 * 5)) / idx * idx; // 예를들어 10의 자리에서 반올림할경우 5를 더하면 올림할 숫자는 앞자리가 바뀌게 된다.
 			idx *= 10;
 		}
-		
+
 		bw.write(n + "");
 
 		bw.flush();
