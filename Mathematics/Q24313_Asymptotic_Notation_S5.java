c0b26d4a2e1a50cce1707f8cd86a5e1bed71f281
YUSHINSHUB <kus9087@naver.com>
2023-07-04 14:07:43 +0900

    230704

--- Mathematics/Q24313_Asymptotic_Notation_S5.java
+++ Mathematics/Q24313_Asymptotic_Notation_S5.java
@@ -1,10 +1,8 @@
-package aaNotepad;
+package Mathematics;
 
 import java.io.*;
-import java.math.*;
-import java.util.*;
 
-public class notepad {
+public class Q24313_Asymptotic_Notation_S5 {
 
 	public static void main(String[] args) throws IOException {
 
