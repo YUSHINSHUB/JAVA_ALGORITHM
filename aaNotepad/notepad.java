0b30420916e9c3eec6a1bc0915df2b93db155efa
YUSIN <kus9087@naver.com>
2023-08-07 23:32:54 +0900

    230807

--- aaNotepad/notepad.java
+++ aaNotepad/notepad.java
@@ -12,52 +12,38 @@ public class notepad {
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
 		int n = Integer.parseInt(br.readLine());
-		Deque<Integer> stk = new LinkedList<>();
+		ArrayList<Integer> qs = new ArrayList<>();
+		int num[] = new int[n];
 
+		String inp[] = br.readLine().split(" ");
 		for (int i = 0; i < n; i++) {
-			String inp[] = br.readLine().split(" ");
-			int c = Integer.parseInt(inp[0]);
+			int t = Integer.parseInt(inp[i]);
+			if (t == 0)
+				qs.add(i);
+		}
 
-			switch (c) {
-			case 1:
-				stk.addFirst(Integer.parseInt(inp[1]));
+		inp = br.readLine().split(" ");
+		for (int i = 0; i < n; i++)
+			num[i] = Integer.parseInt(inp[i]);
+
+		int m = Integer.parseInt(br.readLine());
+		inp = br.readLine().split(" ");
+
+		Collections.reverse(qs);
+
+		for (int i : qs) {
+			bw.write(num[i] + " ");
+			m--;
+			if (m == 0)
 				break;
-			case 2:
-				stk.addLast(Integer.parseInt(inp[1]));
-				break;
-			case 3:
-				if (stk.isEmpty())
-					bw.write("-1\n");
-				else
-					bw.write(stk.pollFirst() + "\n");
-				break;
-			case 4:
-				if (stk.isEmpty())
-					bw.write("-1\n");
-				else
-					bw.write(stk.pollLast() + "\n");
-				break;
-			case 5:
-				bw.write(stk.size() + "\n");
-				break;
-			case 6:
-				if (stk.isEmpty())
-					bw.write("1\n");
-				else
-					bw.write("0\n");
-				break;
-			case 7:
-				if (stk.isEmpty())
-					bw.write("-1\n");
-				else
-					bw.write(stk.peekFirst() + "\n");
-				break;
-			case 8:
-				if (stk.isEmpty())
-					bw.write("-1\n");
-				else
-					bw.write(stk.peekLast() + "\n");
-				break;
+		}
+
+		if (m > 0) {
+			for (String i : inp) {
+				bw.write(i + " ");
+				m--;
+				if (m == 0)
+					break;
 			}
 		}
 
