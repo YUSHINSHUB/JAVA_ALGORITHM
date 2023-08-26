2c8769c2421508921fc88929cdc1c82239895063
YUSIN <kus9087@naver.com>
2023-08-26 19:52:41 +0900

    230826

--- aaNotepad/notepad.java
+++ aaNotepad/notepad.java
@@ -11,28 +11,72 @@ public class notepad {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-		String inp[] = br.readLine().split(" ");
-		int n = Integer.parseInt(inp[0]);
-		int k = Integer.parseInt(inp[1]);
+		String s = br.readLine();
+		int len = s.length();
+		int y = 0, n = 0;
+		int res = 0;
+		boolean pass = false;
 
-		int coin[] = new int[n];
-		int dp[] = new int[10001];
-		Arrays.fill(dp, 0);
-		dp[0] = 1;
+		if (s.charAt(0) == '0')
+			bw.write("0");
+		else if (len == 1)
+			bw.write("1");
+		else {
+			int temp = Integer.parseInt(s.substring(0, 2));
 
-		for (int i = 0; i < n; i++) {
-			coin[i] = Integer.parseInt(br.readLine());
-		}
+			if (temp % 10 == 0 && temp >= 30) {
+				pass = true;
+				res = 0;
+			}
 
-		for (int j = 0; j < n; j++) {
-			for (int i = 1; i <= k; i++) {
-				if (coin[j] <= i) {
-					dp[i] += dp[i - coin[j]];
+			if (!pass) {
+				if (temp <= 26) {
+					res = 2;
+					y = 1;
+					n = 1;
+					if (temp == 10 || temp == 20) {
+						res = 1;
+						y = 1;
+						n = 0;
+					}
+				}else {
+					res = 1;
+					y = 0;
+					n = 1;
+				}
+				
+				for (int i = 1; i < s.length() - 1; i++) {
+					temp = Integer.parseInt(s.substring(i, i + 2));
+
+					if ((temp % 10 == 0 && temp >= 30) || temp == 0) {
+						pass = true;
+						res = 0;
+						break;
+					}
+
+					if (temp <= 26 && temp >= 10) {
+						if (temp == 10 || temp == 20) {
+							res = ((res + 1000000) - y) % 1000000;
+							y = res;
+							n = 0;
+						} else {
+							y = n;
+							n = res;
+							res = (y + n) % 1000000;
+						}
+					} else {
+						y = 0;
+						n = res;
+					}
 				}
 			}
-		}
+			if (res == 0 && !pass)
+				res = 1;
 
-		bw.write(dp[k] + "");
+			temp = Integer.parseInt(s.substring(s.length() - 2, s.length()));
+
+			bw.write(res + "");
+		}
 
 		bw.flush();
 		bw.close();
