774ab7e2a45b8f91a680c359de5ce95ae35183da
USHIN KIM <kus9087@naver.com>
2023-09-14 15:28:03 +0900

    230914

--- aaNotepad/notepad.java
+++ aaNotepad/notepad.java
@@ -11,35 +11,58 @@ public class notepad {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-		int n, m;
-		String inp[] = br.readLine().split(" ");
-		n = Integer.parseInt(inp[0]);
-		m = Integer.parseInt(inp[1]);
+		String s;
+		int idx = 0;
+		StringBuilder res = new StringBuilder("");
 
-		int x1 = -1;
-		int x2 = -1;
-		int y1 = -1;
-		int y2 = -1;
+		while (true) {
 
-		for (int i = 0; i < n; i++) {
-			inp = br.readLine().split(" ");
-			for (int j = 0; j < m; j++) {
-				if (inp[j].equals("1")) {
-					if (x1 == -1) {
-						x1 = j;
-						y1 = i;
-					} else {
-						x2 = j;
-						y2 = i;
-						break;
-					}
-				}
+			s = br.readLine();
+			if (s == null || s.isEmpty())
+				break;
+
+			String inp[] = s.split(" ");
+			double x1, x2, x3, x4;
+			double y1, y2, y3, y4;
+
+			x1 = Double.parseDouble(inp[0]);
+			y1 = Double.parseDouble(inp[1]);
+			x2 = Double.parseDouble(inp[2]);
+			y2 = Double.parseDouble(inp[3]);
+			x3 = Double.parseDouble(inp[4]);
+			y3 = Double.parseDouble(inp[5]);
+			x4 = Double.parseDouble(inp[6]);
+			y4 = Double.parseDouble(inp[7]);
+
+			if (x1 == x2 && y1 == y2) {
+				x3 += x4 - x1;
+				y3 += y4 - y1;
+				res.append(String.format("%.3f %.3f\n", x3, y3));
+			} else if (x1 == x3 && y1 == y3) {
+				x2 += x4 - x1;
+				y2 += y4 - y1;
+				res.append(String.format("%.3f %.3f\n", x2, y2));
+			} else if (x1 == x4 && y1 == y4) {
+				x3 += x2 - x1;
+				y3 += y2 - y1;
+				res.append(String.format("%.3f %.3f\n", x3, y3));
+			} else if (x2 == x3 && y2 == y3) {
+				x1 += x4 - x2;
+				y1 += y4 - y2;
+				res.append(String.format("%.3f %.3f\n", x1, y1));
+			} else if (x2 == x4 && y2 == y4) {
+				x1 += x3 - x2;
+				y1 += y3 - y2;
+				res.append(String.format("%.3f %.3f\n", x1, y1));
+			} else {
+				x1 += x2 - x3;
+				y1 += y2 - y3;
+				res.append(String.format("%.3f %.3f\n", x1, y1));
 			}
-			if (x2 >= 0)
+			idx++;
+			if (idx == 8)
 				break;
 		}
-
-		bw.write(Math.abs(x2 - x1) + Math.abs(y2 - y1) + "");
 
 		bw.flush();
 		bw.close();
