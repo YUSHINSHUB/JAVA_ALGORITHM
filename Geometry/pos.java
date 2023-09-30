43381df4f798cbd299d89f88a07e58639dd804be
USHIN KIM <kus9087@naver.com>
2023-10-01 01:46:36 +0900

    230930

--- Geometry/pos.java
+++ Geometry/pos.java
@@ -1,5 +1,7 @@
 package Geometry;
 
+import java.util.Objects;
+
 public class pos {
 	int x;
 	int y;
@@ -8,4 +10,21 @@ public class pos {
 		this.x = x;
 		this.y = y;
 	}
+	
+	// 객체 간의 동일성을 확인 (Check for object equality)
+		@Override
+		public boolean equals(Object o) {
+			if (this == o)
+				return true;
+			if (o == null || getClass() != o.getClass())
+				return false;
+			pos position = (pos) o;
+			return x == position.x && y == position.y;
+		}
+
+		// 객체의 해시 코드 생성 (Generate hash code for the object)
+		@Override
+		public int hashCode() {
+			return Objects.hash(x, y);
+		}
 }
