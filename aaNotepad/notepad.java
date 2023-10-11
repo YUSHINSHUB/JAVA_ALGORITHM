1e5e9513222e60ad94f48b4f97830434c3e8df17
YUSIN <kus9087@naver.com>
2023-10-11 12:26:00 +0900

    231011

--- aaNotepad/notepad.java
+++ aaNotepad/notepad.java
@@ -6,127 +6,31 @@ import java.util.*;
 
 public class notepad {
 
-	static int idx = 0;
-	static int n, m;
-	static int sea[][];
-	static int by[] = { 1, -1, -1, 1 };
-	static int bx[] = { 0, 1, -1, -1 };
-
-	static int bfs() {
-
-		Queue<Integer> yq = new LinkedList<>();
-		Queue<Integer> xq = new LinkedList<>();
-
-		for (int i = 0; i < n; i++) {
-			for (int j = 0; j < m; j++) {
-				if (sea[i][j] != 0) {
-					yq.add(i);
-					xq.add(j);
-					break;
-				}
-			}
-			if (!yq.isEmpty())
-				break;
-		}
-
-		if (yq.isEmpty())
-			return 0;
-
-		int tsea[][] = new int[n][m];
-		for (int i = 0; i < n; i++) {
-			tsea[i] = sea[i].clone();
-		}
-		tsea[yq.peek()][xq.peek()] = 0;
-
-		while (!yq.isEmpty()) {
-
-			int y = yq.poll();
-			int x = xq.poll();
-
-			for (int i = 0; i < 4; i++) {
-				y += by[i];
-				x += bx[i];
-				if (y < 0 || x < 0 || y >= n || x >= m)
-					continue;
-
-				if (tsea[y][x] > 0) {
-					yq.add(y);
-					xq.add(x);
-					tsea[y][x] = 0;
-				}
-
-			}
-
-		}
-
-		for (int i = 0; i < n; i++) {
-			for (int j = 0; j < m; j++) {
-				if (tsea[i][j] > 0)
-					return 1;
-			}
-		}
-
-		return 2;
-
-	}
-
-	static void melt() {
-
-		for (int i = 0; i < n; i++) {
-			for (int j = 0; j < m; j++) {
-				if (sea[i][j] == 0) {
-					int y = i;
-					int x = j;
-					for (int k = 0; k < 4; k++) {
-						y += by[k];
-						x += bx[k];
-						if (y < 0 || x < 0 || y >= n || x >= m)
-							continue;
-						else if (sea[y][x] > 0) {
-							sea[y][x]--;
-							if (sea[y][x] == 0)
-								sea[y][x]--;
-						}
-					}
-				} else if (sea[i][j] < 0)
-					sea[i][j]++;
-			}
-		}
-
-	}
-
+	static int n;
+	static int pal[];
+	static int dp[][];
+	static boolean visited[][];
+	
+	
 	public static void main(String[] args) throws IOException {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
+		n = Integer.parseInt(br.readLine());
+		pal = new int[n+1];
+		dp = new int[n+1][n+1];
+		visited = new boolean[n+1][n+1];
+		
+		
 		String inp[] = br.readLine().split(" ");
-		int chk = 2;
-		n = Integer.parseInt(inp[0]);
-		m = Integer.parseInt(inp[1]);
-		sea = new int[n][m];
-
-		for (int i = 0; i < n; i++) {
-			inp = br.readLine().split(" ");
-			for (int j = 0; j < m; j++) {
-				sea[i][j] = Integer.parseInt(inp[j]);
-			}
+		
+		for( int i = 0 ; i < n ; i++ ) {
+			pal[i] = Integer.parseInt(inp[i]);
 		}
-
-		while (true) {
-			chk = bfs();
-			if (chk == 0) {
-				idx = 0;
-				break;
-			} else if (chk == 1)
-				break;
-
-			idx++;
-			melt();
-		}
-
-		bw.write(idx + "");
-
+		
+		
+		
 		bw.flush();
 		bw.close();
 
