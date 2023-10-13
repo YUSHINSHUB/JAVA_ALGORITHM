ea0af5257578a61853ba716bf325baa894a97fa4
USHIN KIM <kus9087@naver.com>
2023-10-13 14:43:00 +0900

    231013

--- aaNotepad/notepad.java
+++ aaNotepad/notepad.java
@@ -6,61 +6,38 @@ import java.util.*;
 
 public class notepad {
 
+	static int dp[];
+	static boolean visited[];
+
+	static int recur(int idx) {
+
+		if (visited[idx])
+			return dp[idx];
+		else {
+			visited[idx] = true;
+			dp[idx] = recur(idx - 2) * 3;
+			for (int i = idx - 4; i >= 0; i -= 2) {
+				dp[idx] += recur(i) * 2;
+			}
+			return dp[idx];
+		}
+	}
+
 	public static void main(String[] args) throws IOException {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
 		int n = Integer.parseInt(br.readLine());
-		int res = 0;
-		int bi[] = new int[n];
-		int mem[][] = new int[n][2];
-		int chk[] = new int[1001];
-		Arrays.fill(chk, -1);
-		chk[0] = 0;
+		dp = new int[n + 1];
+		visited = new boolean[n + 1];
+		dp[0] = 1;
+		visited[0] = true;
 
-		String inp[] = br.readLine().split(" ");
-		for (int i = 0; i < n; i++) {
-			bi[i] = Integer.parseInt(inp[i]);
-
-			for (int j = bi[i] - 1; j >= 0; j--) {
-				if (chk[j] >= 0) {
-					chk[bi[i]] = chk[j] + 1;
-					mem[i][0] = chk[bi[i]];
-					break;
-				}
-			}
-
-			if (i == 0) {
-				chk[bi[i]] = 1;
-				mem[i][0] = 1;
-			}
-
-		}
-
-		Arrays.fill(chk, -1);
-		chk[0] = 0;
-
-		for (int i = n - 1; i >= 0; i--) {
-
-			for (int j = bi[i] - 1; j >= 0; j--) {
-				if (chk[j] >= 0) {
-					chk[bi[i]] = chk[j] + 1;
-					mem[i][1] = chk[bi[i]];
-					break;
-				}
-			}
-
-			if (i == n - 1) {
-				chk[bi[i]] = 1;
-				mem[i][1] = 1;
-			}
-
-			if (mem[i][0] + mem[i][1] > res)
-				res = mem[i][0] + mem[i][1] - 1;
-		}
-
-		bw.write(res + "");
+		if (n % 2 != 0)
+			bw.write("0");
+		else
+			bw.write(recur(n) + "");
 
 		bw.flush();
 		bw.close();
