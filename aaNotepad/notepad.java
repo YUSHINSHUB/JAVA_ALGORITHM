afd3a27a8f51dc2824777b8852f1253c15b19bd6
USHIN KIM <kus9087@naver.com>
2023-10-24 18:07:40 +0900

    231024

--- aaNotepad/notepad.java
+++ aaNotepad/notepad.java
@@ -6,49 +6,91 @@ import java.util.*;
 
 public class notepad {
 
+	static int board[][];
+	static int N;
+	static int res = 0;
+
+	static void bt(int idx) {
+		boolean pass = false;
+
+		if (idx == N) {
+			res++;
+			return;
+		}
+
+		for (int j = 0; j < N; j++) {
+			pass = false;
+
+			for (int k = j - 1; k >= 0; k--) {
+				if (board[idx][k] == 1) {
+					pass = true;
+					break;
+				}
+			}
+
+			if (pass)
+				continue;
+
+			for (int k = idx - 1; k >= 0; k--) {
+				if (board[k][j] == 1) {
+					pass = true;
+					break;
+				}
+			}
+
+			if (pass)
+				continue;
+
+			int yt = idx - 1;
+			int xt = j - 1;
+
+			while (yt >= 0 && xt >= 0) {
+				if (board[yt][xt] == 1) {
+					pass = true;
+					break;
+				}
+				yt--;
+				xt--;
+			}
+
+			if (pass)
+				continue;
+
+			yt = idx - 1;
+			xt = j + 1;
+
+			while (yt >= 0 && xt < N) {
+				if (board[yt][xt] == 1) {
+					pass = true;
+					break;
+				}
+				yt--;
+				xt++;
+			}
+
+			if (pass)
+				continue;
+
+			board[idx][j] = 1;
+			bt(idx + 1);
+			board[idx][j] = 0;
+
+		}
+
+	}
+
 	public static void main(String[] args) throws IOException {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-		int N = Integer.parseInt(br.readLine());
-		String inp[] = new String[N];
-		int res = 0;
-		int cnt[] = new int[26];
-		int conv[] = new int[26];
-		int idx = 9;
-		Arrays.fill(cnt, 0);
+		N = Integer.parseInt(br.readLine());
+		board = new int[N][N];
 
-		for (int i = 0; i < N; i++) {
-			inp[i] = br.readLine();
-			for (int j = 0; j < inp[i].length(); j++) {
-				cnt[inp[i].charAt(j) - 'A'] += Math.pow(10, inp[i].length() - j);
-			}
-		}
-
-		while (true) {
-			int hig = 0;
-			int hidx = -1;
-			for (int i = 0; i < 26; i++) {
-				if (cnt[i] > hig) {
-					hig = cnt[i];
-					hidx = i;
-				}
-			}
-			if (hig == 0)
-				break;
-			conv[hidx] = idx;
-			cnt[hidx] = 0;
-			idx--;
-		}
-
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < inp[i].length(); j++) {
-				res += Math.pow(10, inp[i].length() - j-1) * conv[inp[i].charAt(j) - 'A'];
-			}
-		}
+		bt(0);
 
 		bw.write(res + "");
+
 		bw.flush();
 		bw.close();
 
