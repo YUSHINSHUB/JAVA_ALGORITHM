9e7a760993bf887fb0c702f18dca825f65455db2
USHIN KIM <kus9087@naver.com>
2023-10-26 12:02:10 +0900

    231026

--- aaNotepad/notepad.java
+++ aaNotepad/notepad.java
@@ -6,87 +6,45 @@ import java.util.*;
 
 public class notepad {
 
-	static int V, E;
-	static Queue<Integer>[] q = new LinkedList[20001];
-	static int chk[] = new int[20001];
-
-	static boolean bfs() {
-		boolean res = true;
-		int idx = 0;
-		Queue<Integer> bq = new LinkedList<>();
-		for (int i = 1; i <= V; i++) {
-			if (chk[i] == 0) {
-				idx = 1;
-				chk[i] = 1;
-				bq.add(i);
-				while (!bq.isEmpty()) {
-					int temp = bq.poll();
-					while (!q[temp].isEmpty()) {
-						int t = q[temp].poll();
-
-						if (chk[t] == 0) {
-							if (idx == 1) {
-								chk[t] = 2;
-								bq.add(t);
-							} else {
-								bq.add(t);
-								chk[t] = 1;
-							}
-						} else if ((idx == 1 && chk[t] == 1) || (idx == 2 && chk[t] == 2)) {
-							res = false;
-							break;
-						}
-					}
-
-					if (!res)
-						break;
-
-					if (idx == 1)
-						idx = 2;
-					else
-						idx = 1;
-				}
-				if (!res)
-					break;
-				idx = 0;
-			}
-		}
-		return res;
-
-	}
-
 	public static void main(String[] args) throws IOException {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-		int K = Integer.parseInt(br.readLine());
+		HashMap<Integer, Integer> hm = new HashMap<>();
+		int N = Integer.parseInt(br.readLine());
+		int nums[] = new int[N];
+		int res = 0;
 
-		for (int i = 0; i < K; i++) {
-			String inp[] = br.readLine().split(" ");
-			V = Integer.parseInt(inp[0]);
-			E = Integer.parseInt(inp[1]);
-			Arrays.fill(chk, 0);
+		String inp[] = br.readLine().split(" ");
 
-			for (int j = 1; j <= V; j++)
-				q[j] = new LinkedList<>();
+		for (int i = 0; i < N; i++) {
+			nums[i] = Integer.parseInt(inp[i]);
+		}
 
-			for (int j = 0; j < E; j++) {
-				inp = br.readLine().split(" ");
-				int a = Integer.parseInt(inp[0]);
-				int b = Integer.parseInt(inp[1]);
+		for (int i = 0; i < N; i++) {
+			int s = 0;
+			int e = N - 1;
+			while (true) {
+				if (s == i)
+					s++;
+				if (e == i)
+					e--;
+				if (s >= e)
+					break;
 
-				q[a].add(b);
-				q[b].add(a);
-			}
+				if (nums[s] + nums[e] == nums[i]) {
+					res++;
+					break;
+				} else if (nums[s] + nums[e] < nums[i])
+					s++;
+				else
+					e--;
 
-			if (bfs()) {
-				bw.write("YES\n");
-			} else {
-				bw.write("NO\n");
 			}
 		}
 
+		bw.write(res + "");
 		bw.flush();
 		bw.close();
 
