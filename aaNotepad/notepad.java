fbf49886424d813c8f8e5d11ffd1808c515a9fc1
USHIN KIM <kus9087@naver.com>
2023-11-22 17:03:37 +0900

    231122

--- aaNotepad/notepad.java
+++ aaNotepad/notepad.java
@@ -3,96 +3,45 @@ package aaNotepad;
 import java.io.*;
 import java.util.*;
 
-class route {
-	int to, lim;
-
-	route(int to, int lim) {
-		this.to = to;
-		this.lim = lim;
-	}
-}
-
 public class notepad {
-
-	static int N, M;
-	static boolean visited[];
-	static int f1, f2;
-	static int hig = Integer.MIN_VALUE;
-	static int low = Integer.MAX_VALUE;
-	static int mid = 0;
-	static ArrayList<route>[] list;
-
-	static boolean search(int idx) {
-
-		Arrays.fill(visited, false);
-		visited[f1] = true;
-		Queue<Integer> q = new LinkedList<>();
-		q.add(f1);
-		while (!q.isEmpty()) {
-			int t = q.poll();
-			if (t == f2) {
-				return true;
-			}
-			for (int i = 0; i < list[t].size(); i++) {
-				if (list[t].get(i).lim >= idx && !visited[list[t].get(i).to]) {
-					q.add(list[t].get(i).to);
-					visited[list[t].get(i).to] = true;
-				}
-			}
-
-		}
-
-		return false;
-
-	}
 
 	public static void main(String[] args) throws IOException {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-		int res = 0;
+		StringBuilder sb = new StringBuilder("");
 		String inp[] = br.readLine().split(" ");
-		N = Integer.parseInt(inp[0]);
-		M = Integer.parseInt(inp[1]);
-		visited = new boolean[N + 1];
-		list = new ArrayList[N + 1];
+		int N = Integer.parseInt(inp[0]);
+		int K = Integer.parseInt(inp[1]);
 
-		Arrays.fill(visited, false);
-		for (int i = 1; i <= N; i++) {
-			list[i] = new ArrayList<>();
-		}
-		for (int i = 0; i < M; i++) {
-			inp = br.readLine().split(" ");
-			int A = Integer.parseInt(inp[0]);
-			int B = Integer.parseInt(inp[1]);
-			int C = Integer.parseInt(inp[2]);
+		String num = br.readLine();
+		Stack<Character> stk = new Stack<>();
 
-			list[A].add(new route(B, C));
-			list[B].add(new route(A, C));
+		for (int i = 0; i < N - 1; i++) {
 
-			hig = Math.max(hig, C);
-			low = Math.min(low, C);
-		}
-
-		mid = (low + hig) / 2;
-
-		inp = br.readLine().split(" ");
-		f1 = Integer.parseInt(inp[0]);
-		f2 = Integer.parseInt(inp[1]);
-		visited[f1] = true;
-
-		while (low <= hig) {
-			mid = (low + hig) / 2;
-			if (search(mid)) {
-				low = mid + 1;
-				res = mid;
-			} else {
-				hig = mid - 1;
+			stk.add(num.charAt(i));
+			while (!stk.isEmpty() && K > 0) {
+				if (stk.peek() < num.charAt(i + 1)) {
+					stk.pop();
+					K--;
+				} else
+					break;
 			}
 		}
+		stk.add(num.charAt(N - 1));
 
-		bw.write(res + "");
+		while (K != 0) {
+			stk.pop();
+			K--;
+		}
+
+		while (!stk.isEmpty()) {
+			sb.append(stk.pop());
+		}
+
+		bw.write(sb.reverse().toString());
+
 		bw.flush();
 		bw.close();
 
