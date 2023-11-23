4fccd38262f69abb31c30d920cb232493cf05dbc
USHIN KIM <kus9087@naver.com>
2023-11-23 12:12:53 +0900

    231123

--- aaNotepad/notepad.java
+++ aaNotepad/notepad.java
@@ -5,42 +5,103 @@ import java.util.*;
 
 public class notepad {
 
+	static int N, M;
+	static int grp[][];
+	static int visited[][];
+	static int ny[] = { -1, 1, 0, 1 };
+	static int nx[] = { 0, -1, 2, -1 };
+
+	static void search() {
+
+		Queue<Integer> yq = new LinkedList<>();
+		Queue<Integer> xq = new LinkedList<>();
+		Queue<Integer> wyq = new LinkedList<>();
+		Queue<Integer> wxq = new LinkedList<>();
+		yq.add(0);
+		xq.add(0);
+
+		while (!yq.isEmpty()) {
+			int cy = yq.poll();
+			int cx = xq.poll();
+
+			int y = cy;
+			int x = cx;
+
+			for (int i = 0; i < 4; i++) {
+				y += ny[i];
+				x += nx[i];
+				if (y < 0 || x < 0 || y >= N || x >= M)
+					continue;
+				else if (visited[y][x] >= 0)
+					continue;
+				else if (grp[y][x] == 1) {
+					visited[y][x] = visited[cy][cx] + 1;
+					wyq.add(y);
+					wxq.add(x);
+					continue;
+				}
+
+				visited[y][x] = visited[cy][cx] + 1;
+				yq.add(y);
+				xq.add(x);
+
+			}
+		}
+
+		while (!wyq.isEmpty()) {
+			yq.add(wyq.poll());
+			xq.add(wxq.poll());
+
+			while (!yq.isEmpty()) {
+				int cy = yq.poll();
+				int cx = xq.poll();
+
+				int y = cy;
+				int x = cx;
+
+				for (int i = 0; i < 4; i++) {
+					y += ny[i];
+					x += nx[i];
+					if (y < 0 || y >= N || x < 0 || x >= M)
+						continue;
+					else if (grp[y][x] == 1)
+						continue;
+					else if (visited[y][x] >= 0 && visited[y][x] <= visited[cy][cx] + 1)
+						continue;
+
+					visited[y][x] = visited[cy][cx] + 1;
+					yq.add(y);
+					xq.add(x);
+				}
+			}
+		}
+
+	}
+
 	public static void main(String[] args) throws IOException {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-		StringBuilder sb = new StringBuilder("");
 		String inp[] = br.readLine().split(" ");
-		int N = Integer.parseInt(inp[0]);
-		int K = Integer.parseInt(inp[1]);
+		N = Integer.parseInt(inp[0]);
+		M = Integer.parseInt(inp[1]);
+		grp = new int[N][M];
+		visited = new int[N][M];
 
-		String num = br.readLine();
-		Stack<Character> stk = new Stack<>();
-
-		for (int i = 0; i < N - 1; i++) {
-
-			stk.add(num.charAt(i));
-			while (!stk.isEmpty() && K > 0) {
-				if (stk.peek() < num.charAt(i + 1)) {
-					stk.pop();
-					K--;
-				} else
-					break;
-			}
-		}
-		stk.add(num.charAt(N - 1));
-
-		while (K != 0) {
-			stk.pop();
-			K--;
+		for (int i = 0; i < N; i++) {
+			String temp = br.readLine();
+			Arrays.fill(visited[i], -1);
+			for (int j = 0; j < M; j++)
+				grp[i][j] = Integer.parseInt(Character.toString(temp.charAt(j)));
 		}
 
-		while (!stk.isEmpty()) {
-			sb.append(stk.pop());
-		}
+		visited[N-1][M-1] = -2;
+		visited[0][0] = 0;
+		
+		search();
 
-		bw.write(sb.reverse().toString());
+		bw.write(visited[N - 1][M - 1] + 1 + "");
 
 		bw.flush();
 		bw.close();
